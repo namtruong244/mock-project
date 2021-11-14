0593befd46950b1724584e2da57de016064f2325
kyn7299 <nhatuyen070299@gmail.com>
2021-11-14 18:01:31 +0700

    add Delete Product

--- src/app/features/products/pages/CardFood.js
+++ src/app/features/products/pages/CardFood.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React } from 'react'
 
 import {
   Badge,
@@ -9,9 +9,14 @@ import {
   Flex,
   Icon,
   Image,
-  Spacer,
   Tooltip,
   useColorModeValue,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  Text,
 } from '@chakra-ui/react'
 import { FiShoppingCart } from 'react-icons/fi'
 import { currencyFormatter } from '../../../utils'
@@ -24,8 +29,8 @@ import { productModalActions } from '../../stores'
 import { productService } from '../../../services'
 import { useMutation } from 'react-query'
 
-export function CardFood({ item }) {
-  const {data, mutate} = useMutation(productService.deleteProduct)
+export function CardFood({ item, deleteProduct, isLoadingDelete }) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const currentUser = useSelector(({ auth }) => auth.currentUser)
   const isShop = currentUser?.role === CmnConst.SHOP_ROLE
   const dispatch = useDispatch()
@@ -37,28 +42,49 @@ export function CardFood({ item }) {
   const deleteProductHandler = () => {
     const data = {
       shopId: currentUser.userId,
-      itemId: item.itemId
+      itemId: item.itemId,
     }
-    mutate(data)
+    deleteProduct(data)
   }
 
   return (
-    <React.Fragment>
-      <Flex w='full'>
+    <>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          {/* <ModalCloseButton /> */}
+          <ModalBody py={8}>
+            <Flex justifyContent="center">
+              <Text fontWeight="500" fontSize="22px">
+                Are you sure to delete this item
+              </Text>
+            </Flex>
+            <Flex justifyContent="center" mt={5}>
+              <Button colorScheme="red" onClick={deleteProductHandler} isLoading={isLoadingDelete}>
+                Delete
+              </Button>
+              <Button colorScheme="blue" ml={3} onClick={onClose}>
+                Cancel
+              </Button>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      <Flex w="full">
         <Box
           bg={useColorModeValue('white', 'gray.800')}
-          maxW='sm'
-          borderWidth='1px'
-          rounded='lg'
-          shadow='lg'
-          position='relative'
+          maxW="sm"
+          borderWidth="1px"
+          rounded="lg"
+          shadow="lg"
+          position="relative"
         >
           <Circle
-            size='10px'
-            position='absolute'
+            size="10px"
+            position="absolute"
             top={2}
             right={2}
-            bg='red.200'
+            bg="red.200"
           />
 
           <Image
@@ -66,87 +92,101 @@ export function CardFood({ item }) {
             height={'11vw'}
             minW={'16vw'}
             maxW={'16vw'}
-            objectFit='cover'
+            objectFit="cover"
             src={`${CmnConst.BASE_64_PREFIX}${item.image}`}
             alt={`Picture of ${item.name}`}
-            roundedTop='lg'
+            roundedTop="lg"
           />
 
-          <Box p='6'>
-            <Box d='flex' alignItems='baseline'>
-              <Badge rounded='full' px='2' fontSize='0.8em' colorScheme='red'>
+          <Box p="6">
+            <Box d="flex" alignItems="baseline">
+              <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="red">
                 New
               </Badge>
             </Box>
-            <Flex mt='1' justifyContent='space-between' alignContent='center'>
+            <Flex mt="1" justifyContent="space-between" alignContent="center">
               <Box
                 maxW={'140px'}
-                fontSize='xl'
-                fontWeight='semibold'
-                as='h4'
-                lineHeight='tight'
+                fontSize="xl"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
                 isTruncated
               >
                 {item.name}
               </Box>
-              {!isShop &&
-              <Tooltip
-                label='Add to cart'
-                bg='white'
-                placement={'top'}
-                color={'gray.800'}
-                fontSize={'1em'}
-              >
-                <chakra.a href={'#'} display={'flex'}>
-                  <Icon as={FiShoppingCart} h={7} w={7} alignSelf={'center'} />
-                </chakra.a>
-              </Tooltip>
-              }
+              {!isShop && (
+                <Tooltip
+                  label="Add to cart"
+                  bg="white"
+                  placement={'top'}
+                  color={'gray.800'}
+                  fontSize={'1em'}
+                >
+                  <chakra.a href={'#'} display={'flex'}>
+                    <Icon
+                      as={FiShoppingCart}
+                      h={7}
+                      w={7}
+                      alignSelf={'center'}
+                    />
+                  </chakra.a>
+                </Tooltip>
+              )}
             </Flex>
 
-            <Flex justifyContent='space-between' alignContent='center'>
+            <Flex justifyContent="space-between" alignContent="center">
               <Rating />
             </Flex>
             <Flex>
               <Box
-                fontSize='2xl'
+                fontSize="2xl"
                 color={useColorModeValue('gray.800', 'white')}
                 mt={2}
               >
                 {currencyFormatter.format(item.price)}
               </Box>
             </Flex>
-            {isShop && item.shopId === currentUser.userId &&
-            <Flex mt={2} justifyContent={'end'}>
-              <Box>
-                <Tooltip
-                  label='Edit product'
-                  bg='white'
-                  placement={'top'}
-                  color={'gray.800'}
-                  fontSize={'1em'}
-                >
-                  <Button color='pink.300' variant='outline' onClick={editProductHandler}>
-                    <Icon as={EditIcon} h={4} w={4} alignSelf={'center'} />
-                  </Button>
-                </Tooltip>
-                <Tooltip
-                  label='Delete product'
-                  bg='white'
-                  placement={'top'}
-                  color={'gray.800'}
-                  fontSize={'1em'}
-                >
-                  <Button ml={2} color='red.400' variant='outline' onClick={deleteProductHandler}>
-                    <Icon as={DeleteIcon} h={4} w={4} alignSelf={'center'} />
-                  </Button>
-                </Tooltip>
-              </Box>
-            </Flex>
-            }
+            {isShop && item.shopId === currentUser.userId && (
+              <Flex mt={2} justifyContent={'end'}>
+                <Box>
+                  <Tooltip
+                    label="Edit product"
+                    bg="white"
+                    placement={'top'}
+                    color={'gray.800'}
+                    fontSize={'1em'}
+                  >
+                    <Button
+                      color="pink.300"
+                      variant="outline"
+                      onClick={editProductHandler}
+                    >
+                      <Icon as={EditIcon} h={4} w={4} alignSelf={'center'} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip
+                    label="Delete product"
+                    bg="white"
+                    placement={'top'}
+                    color={'gray.800'}
+                    fontSize={'1em'}
+                  >
+                    <Button
+                      ml={2}
+                      color="red.400"
+                      variant="outline"
+                      onClick={onOpen}
+                    >
+                      <Icon as={DeleteIcon} h={4} w={4} alignSelf={'center'} />
+                    </Button>
+                  </Tooltip>
+                </Box>
+              </Flex>
+            )}
           </Box>
         </Box>
       </Flex>
-    </React.Fragment>
+    </>
   )
 }
--- src/app/features/user/pages/ProfilePage.js
+++ src/app/features/user/pages/ProfilePage.js
@@ -1,13 +1,21 @@
 import ProfileCard from '../components/ProfileCard'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import { useQuery } from 'react-query'
-import { shopService } from '../../../services'
+import { useMutation, useQuery } from 'react-query'
+import { productService, shopService } from '../../../services'
 import { Box, CircularProgress, Flex, Stack } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 import { history } from '../../../utils'
 import { StoreDetail } from '../../stores'
-import { Container, Next, PageGroup, Paginator, Previous, usePaginator } from 'chakra-paginator'
+import { useToast } from '@chakra-ui/react'
+import {
+  Container,
+  Next,
+  PageGroup,
+  Paginator,
+  Previous,
+  usePaginator,
+} from 'chakra-paginator'
 
 export default function ProfilePage() {
   const { userId } = useParams()
@@ -15,34 +23,61 @@ export default function ProfilePage() {
   const { data, isLoading, refetch, isError } = useQuery('profile', () =>
     shopService.getInfoById(userId)
   )
+  const {
+    data: deleteData,
+    isLoading: isLoadingDelete,
+    mutate,
+  } = useMutation(productService.deleteProduct)
   const [totalProduct, setTotalProduct] = useState()
   const [products, setProducts] = useState([])
+  const toast = useToast()
 
   useEffect(() => {
     refetch()
   }, [userId, currentUser])
+
+  useEffect(() => {
+    if (deleteData?.errorMessage != ' ') {
+      toast({
+        title: 'Success',
+        description: 'Delete Successful!',
+        position: 'top-right',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      })
+      refetch()
+    } else
+      toast({
+        title: 'Fail',
+        description: 'Delete Fail!',
+        position: 'top-right',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+  }, [deleteData])
 
   // constants
   const outerLimit = 2
   const innerLimit = 2
   const itemPerPage = 9
 
-  const {
-    isDisabled,
-    pagesQuantity,
-    currentPage,
-    setCurrentPage
-  } = usePaginator({
-    total: totalProduct,
-    initialState: {
-      pageSize: itemPerPage,
-      currentPage: 1,
-      isDisabled: false,
-    },
-  })
+  const { isDisabled, pagesQuantity, currentPage, setCurrentPage } =
+    usePaginator({
+      total: totalProduct,
+      initialState: {
+        pageSize: itemPerPage,
+        currentPage: 1,
+        isDisabled: false,
+      },
+    })
 
   useEffect(() => {
-    let productSlice = data?.items?.slice((currentPage - 1) * itemPerPage, currentPage * itemPerPage)
+    let productSlice = data?.items?.slice(
+      (currentPage - 1) * itemPerPage,
+      currentPage * itemPerPage
+    )
     productSlice = productSlice?.filter(product => product.isActive)
     setProducts(productSlice)
     setTotalProduct(data?.items?.length)
@@ -50,7 +85,7 @@ export default function ProfilePage() {
 
   if (isLoading) {
     return (
-      <Flex justifyContent='center' alignItems='center' h={'43vh'}>
+      <Flex justifyContent="center" alignItems="center" h={'43vh'}>
         <CircularProgress isIndeterminate color="pink.300" />
       </Flex>
     )
@@ -62,12 +97,21 @@ export default function ProfilePage() {
   }
 
   // handlers
-  const handlePageChange = (nextPage) => {
+  const handlePageChange = nextPage => {
     setCurrentPage(nextPage)
   }
 
+  const deleteProductHandler = async product => {
+    await mutate(product)
+    await refetch()
+  }
+
   return (
-    <Stack direction={{ base: 'column', md: 'row' }} maxW={'1140px'} margin={'0 auto'}>
+    <Stack
+      direction={{ base: 'column', md: 'row' }}
+      maxW={'1140px'}
+      margin={'0 auto'}
+    >
       <Flex flex={0.5} w={'100%'} mr={4}>
         <ProfileCard
           userData={data}
@@ -75,8 +119,14 @@ export default function ProfilePage() {
         />
       </Flex>
       <Flex flex={1.5} mt={3} direction={'column'}>
-        <StoreDetail products={products}/>
-        {totalProduct <= itemPerPage ? '' :
+        <StoreDetail
+          products={products}
+          deleteProduct={deleteProductHandler}
+          isLoadingDelete={isLoadingDelete}
+        />
+        {totalProduct <= itemPerPage ? (
+          ''
+        ) : (
           <Box w={'full'} margin={'0 auto'}>
             <Paginator
               isDisabled={isDisabled}
@@ -89,18 +139,14 @@ export default function ProfilePage() {
               pagesQuantity={pagesQuantity}
               onPageChange={handlePageChange}
             >
-              <Container align='center' justify='space-between' w='full' p={4}>
-                <Previous>
-                  Previous
-                </Previous>
-                <PageGroup isInline align='center' />
-                <Next>
-                  Next
-                </Next>
+              <Container align="center" justify="space-between" w="full" p={4}>
+                <Previous>Previous</Previous>
+                <PageGroup isInline align="center" />
+                <Next>Next</Next>
               </Container>
             </Paginator>
           </Box>
-        }
+        )}
       </Flex>
     </Stack>
   )
